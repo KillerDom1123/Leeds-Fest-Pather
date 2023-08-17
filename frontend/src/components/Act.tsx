ecce962c67f7e9405cb65e071d7a2bc1883f43aa
Killer_Dom1123 <dominic1123@live.co.uk>
2023-08-17 16:52:12 +0100

    Start select acts

--- frontend/src/components/Act.tsx
+++ frontend/src/components/Act.tsx
@@ -8,9 +8,25 @@ type ActProps = {
 };
 
 const Act = ({ name, stage, timeStart, timeEnd }: ActProps) => {
+    const start = new Date(timeStart).toLocaleString('en-GB');
+    const end = new Date(timeEnd).toLocaleString('en-GB');
+
+    // https://react.dev/learn/sharing-state-between-components
+
     return (
         <div className="act-container">
-            <p>{name}</p>
+            <div className="act-photo">
+                <img alt={name} />
+            </div>
+            <div className="info">
+                <div className="name-stage">
+                    <p className="name">{name}</p>
+                    <p className="stage">{stage}</p>
+                </div>
+                <p className="timing">
+                    {start} - {end}
+                </p>
+            </div>
         </div>
     );
 };
